ca8c7a77bee9e7d52156ee97b2f0321506231009
adames88 <adamelshimi1234@hotmail.co.uk>
2025-01-27 11:39:45 +0200

    adding superbase tools for agent

---
+++ app5.py
@@ -0,0 +1,220 @@
+from helper import load_env
+load_env()
+import streamlit as st
+import os
+from flow_pipeline2 import SalesPipeline
+import pandas as pd
+import textwrap
+from IPython.display import HTML
+from flow_pipeline import StreamToExpander
+import sys
+import textwrap
+
+# Set OpenAI Model
+os.environ['OPENAI_MODEL_NAME'] = 'gpt-4o-mini'
+
+# Initialize the SalesPipeline
+flow = SalesPipeline()
+
+
+# Initialize session state
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+if "state" not in st.session_state:
+    st.session_state.state = {
+        "score_crews_results": [],
+        "filtered_leads": [],
+        "emails": [],
+        "cost": [],
+    }
+if "progress" not in st.session_state:
+    st.session_state.progress = st.empty()
+
+# # Helper function: Add to chat history
+def add_to_chat(role, content):
+    st.session_state.messages.append({"role": role, "content": content})
+    with st.chat_message(role):
+        st.markdown(content)
+
+
+# Function to parse pipeline outputs
+def process_pipeline_outputs(emails):
+    """Parse the outputs of tasks from the pipeline and update session state."""
+
+    # Process lead scores
+    scores = flow.state["score_crews_results"]
+    if scores:
+        for i in range(len(scores)):
+            lead_scoring_result = scores[i].pydantic
+
+            # Prepare data for Lead Scores tab
+            lead_data = {
+                'Name': lead_scoring_result.personal_info.name,
+                'Job Title': lead_scoring_result.personal_info.job_title,
+                'Role Relevance': lead_scoring_result.personal_info.role_relevance,
+                'Professional Background': lead_scoring_result.personal_info.professional_background,
+                'Company Name': lead_scoring_result.company_info.company_name,
+                'Industry': lead_scoring_result.company_info.industry,
+                'Company Size': lead_scoring_result.company_info.company_size,
+                'Revenue': lead_scoring_result.company_info.revenue,
+                'Market Presence': lead_scoring_result.company_info.market_presence,
+                'Lead Score': lead_scoring_result.lead_score.score,
+                'Scoring Criteria': ', '.join(lead_scoring_result.lead_score.scoring_criteria),
+                'Validation Notes': lead_scoring_result.lead_score.validation_notes
+            }
+            st.session_state.state["score_crews_results"].append(lead_data)
+
+            # Process filtered leads
+            filtered_leads_data = {
+                'Name': lead_scoring_result.personal_info.name,
+                'Job Title': lead_scoring_result.personal_info.job_title,
+                'Role Relevance': lead_scoring_result.personal_info.role_relevance,
+                'Professional Background': lead_scoring_result.personal_info.professional_background,
+                'Company Name': lead_scoring_result.company_info.company_name,
+                'Industry': lead_scoring_result.company_info.industry,
+                'Validation Notes': lead_scoring_result.lead_score.validation_notes
+            }
+            st.session_state.state["filtered_leads"].append(filtered_leads_data)
+
+    # Process emails
+    #emails = flow.state["emails"]
+    if emails:
+        for email in emails:
+            wrapped_email = textwrap.fill(email.raw, width=80)
+            st.session_state.state["emails"].append(wrapped_email)
+
+        for index, cost in enumerate(st.session_state.state["score_crews_results"]):
+            scores = st.session_state.state["score_crews_results"]
+            lead_scoring_result = scores[index]
+            # Convert UsageMetrics instance to a DataFrame
+            df_usage_scoreLead_metrics = pd.DataFrame([flow.state["score_crews_results"][index].token_usage.dict()])
+            # Convert UsageMetrics instance to a DataFrame
+            df_usage_email_metrics = pd.DataFrame([emails[index].token_usage.dict()])
+            # Calculate total costs
+            costs_score = 0.150 * df_usage_scoreLead_metrics['total_tokens'].sum() / 1_000_000
+            costs_email = 0.150 * df_usage_email_metrics['total_tokens'].sum() / 1_000_000
+            st.session_state.state["cost"].append({f"Lead Name":f"{lead_scoring_result["Name"]} - {lead_scoring_result["Company Name"]}", 
+                                                    "Total Lead Score cost": f"{costs_score:.4f} ($)", 
+                                                    "Total Email Costs": f"{costs_email:.4f} ($)",
+                                                    "Total Costs": str(float(f"{costs_email:.4f}") + float(f"{costs_score:.4f}")) + " $"})
+
+
+
+# Main pipeline execution
+def kickoff_pipeline():
+    with st.spinner("Running the pipeline..."):
+        add_to_chat("assistant", "Starting the pipeline...")
+        emails = flow.kickoff()
+        process_pipeline_outputs(emails)
+        add_to_chat("assistant", "Pipeline execution complete!")
+    return emails 
+
+# Streamlit UI components
+st.title("Lead Scoring and Engagement Dashboard")
+
+# Chat interface for pipeline execution
+st.header("💬 SensAI Agents Interface",divider="green")
+if st.button("Run Pipeline"):
+    with st.status("🤖 **SenAI Agents at work...**", state="running", expanded=True) as status:
+        with st.container(height=500, border=False):
+            sys.stdout = StreamToExpander(st)
+            result = kickoff_pipeline()
+        status.update(label="✅ Email Campaign Ready!",
+                        state="complete", expanded=False)
+    # Add a message indicating completion to the chat history
+    #add_to_chat("assistant", "Pipeline execution complete! You can now explore the results.")
+
+
+# Tabs for parsed outputs
+st.header("Pipeline Outputs")
+tab1, tab2, tab3, tab4= st.tabs(
+    ["Lead Scores", "Filtered Leads", "Generated Emails", "Costs"]
+)
+
+# Tab 1: Lead Scores
+with tab1:
+    st.write("Lead Scores:")
+    if st.session_state.state["score_crews_results"]:
+        for i in range(len(st.session_state.state["score_crews_results"])):
+            lead_scores_df = pd.DataFrame.from_dict(
+                st.session_state.state["score_crews_results"][i], orient="index", columns=["Value"]
+            ).reset_index().rename(columns={"index": "Attribute"})
+            st.table(lead_scores_df)
+
+            # Add a download button
+            csv = lead_scores_df.to_csv(index=False)
+            st.download_button(
+                label="Download Lead Scores as CSV",
+                data=csv,
+                file_name=f"lead_scores_{i+1}.csv",
+                mime="text/csv",
+            )
+    else:
+        st.warning("No scores available yet. Run the pipeline.")
+
+# Tab 2: Filtered Leads
+with tab2:
+    st.write("Filtered Leads:")
+    if st.session_state.state["filtered_leads"]:
+        filtered_leads_df = pd.DataFrame(st.session_state.state["filtered_leads"])
+        st.table(filtered_leads_df)
+
+        # Add a download button
+        csv = filtered_leads_df.to_csv(index=False)
+        st.download_button(
+            label="Download Filtered Leads as CSV",
+            data=csv,
+            file_name="filtered_leads.csv",
+            mime="text/csv",
+        )
+    else:
+        st.warning("No filtered leads available yet. Run the pipeline.")
+
+# Tab 3: Generated Emails
+with tab3:
+    st.write("Generated Emails:")
+    if st.session_state.state["emails"]:
+        all_emails = []
+        if st.session_state.state["emails"]:
+            for index, email in enumerate(st.session_state.state["emails"]):
+                scores = st.session_state.state["score_crews_results"]
+                lead_scoring_result = scores[index]
+                st.text_area(f"Generated Email - {lead_scoring_result['Company Name']}", email, height=150)
+                
+                # Save individual email to a list
+                all_emails.append({"Lead Name": lead_scoring_result['Name'], "Email": email})
+            
+            # Convert emails to a DataFrame
+            emails_df = pd.DataFrame(all_emails)
+            
+            # Add a download button
+            csv = emails_df.to_csv(index=False)
+            st.download_button(
+                label="Download All Emails as CSV",
+                data=csv,
+                file_name="generated_emails.csv",
+                mime="text/csv",
+            )
+        else:
+            st.warning("No emails generated yet. Run the pipeline.")
+
+
+# Tab 4: Costs
+with tab4:
+    st.write("Cost Analysis:")
+    # process costs
+    if st.session_state.state["emails"]:
+            costs_df = pd.DataFrame(st.session_state.state["cost"])
+            st.table(costs_df)
+            
+            # Add a download button
+            csv = costs_df.to_csv(index=False)
+            st.download_button(
+                label="Download Cost Analysis as CSV",
+                data=csv,
+                file_name="cost_analysis.csv",
+                mime="text/csv",
+                )
+    else:
+        st.warning("No usage metrics available yet. Run the pipeline.")
---
+++ flow_pipeline.py
@@ -0,0 +1,355 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task, before_kickoff, after_kickoff
+from crewai_tools import SerperDevTool, ScrapeWebsiteTool
+from pydantic import BaseModel, Field, ConfigDict
+from typing import Dict, Optional, List, Set, Tuple
+import yaml
+from crewai import Flow
+from crewai.flow.flow import listen, start
+import warnings
+warnings.filterwarnings("always", module="pydantic")
+import logging
+import re
+import pandas as pd
+import streamlit as st
+import smtplib
+from email.message import EmailMessage
+import agentops
+#agentops.init("10d2ae41-41a5-468a-a0da-b0ab4225a8b0",skip_auto_end_session=True)
+
+from supabase_tools import (
+    supabase_get_row_tool, 
+    supabase_get_all_rows_tool, 
+    supabase_insert_row_tool, 
+    supabase_delete_row_tool, 
+    supabase_update_row_tool
+)
+
+from crewai_tools import FileWriterTool
+from crewai.knowledge.source.string_knowledge_source import StringKnowledgeSource
+
+text_knowledge = open("knowledge/table_info.txt", "r").read()
+
+file_writer = FileWriterTool(
+    file_path="./knowledge/table_info.txt",
+    name="Table Info"
+)
+
+# Create instances of the tools
+get_row_tool = supabase_get_row_tool()  # Instantiate the tool
+get_all_rows = supabase_get_all_rows_tool()
+insert_row = supabase_insert_row_tool()
+delete_row = supabase_delete_row_tool()
+update_row = supabase_update_row_tool()
+
+logging.basicConfig(
+    level=logging.DEBUG
+) 
+
+class LeadPersonalInfo(BaseModel):
+    name: str = Field(description="The full name of the lead.")
+    job_title: str = Field(description="The job title of the lead.")
+    role_relevance: int = Field(ge=0, le=10, description="A score representing how relevant the lead's role is to the decision-making process (0-10).")
+    professional_background: Optional[str] = Field(description="A brief description of the lead's professional background.")
+
+class CompanyInfo(BaseModel):
+    company_name: str = Field(description="The name of the company the lead works for.")
+    industry: str = Field(description="The industry in which the company operates.")
+    company_size: int = Field(description="The size of the company in terms of employee count.")
+    revenue: Optional[float] = Field(None, description="The annual revenue of the company, if available.")
+    market_presence: int = Field(ge=0, le=10, description="A score representing the company's market presence (0-10).")
+
+class LeadScore(BaseModel):
+    score: int = Field(ge=0, le=100, description="The final score assigned to the lead (0-100).")
+    scoring_criteria: List[str] = Field(description="The criteria used to determine the lead's score.")
+    validation_notes: Optional[str] = Field(None, description="Any notes regarding the validation of the lead score.")
+
+class LeadScoringResult(BaseModel):
+    personal_info: LeadPersonalInfo = Field(description="Personal information about the lead.")
+    company_info: CompanyInfo = Field(description="Information about the lead's company.")
+    lead_score: LeadScore = Field(description="The calculated score and related information for the lead.")
+
+
+        # Define file paths for YAML configurations
+files = {
+        'agents': 'config/agents.yaml',
+        'tasks': 'config/tasks.yaml'
+    }
+# Load configurations from YAML files
+configs = {}
+for config_type, file_path in files.items():
+    with open(file_path, 'r') as file:
+        configs[config_type] = yaml.safe_load(file)
+
+agents_config = configs['agents']
+tasks_config = configs['tasks']
+
+class StreamToExpander:
+    def __init__(self, expander):
+        self.expander = expander
+        self.buffer = []
+        self.colors = ['red', 'green', 'blue', 'orange', "yellow","pink", "gray"]  # Define a list of colors
+        self.color_index = 0  # Initialize color index
+
+    def write(self, data):
+        # Filter out ANSI escape codes using a regular expression
+        cleaned_data = re.sub(r'\x1B\[[0-9;]*[mK]', '', data)
+
+        # Check if the data contains 'task' information
+        task_match_object = re.search(r'\"task\"\s*:\s*\"(.*?)\"', cleaned_data, re.IGNORECASE)
+        task_match_input = re.search(r'task\s*:\s*([^\n]*)', cleaned_data, re.IGNORECASE)
+        task_value = None
+        if task_match_object:
+            task_value = task_match_object.group(1)
+        elif task_match_input:
+            task_value = task_match_input.group(1).strip()
+
+        if task_value:
+            st.toast(":robot_face: " + task_value)
+
+        # Check if the text contains the specified phrase and apply color
+        if "Entering new CrewAgentExecutor chain" in cleaned_data:
+            # Apply different color and switch color index
+            self.color_index = (self.color_index + 1) % len(self.colors)  # Increment color index and wrap around if necessary
+
+            cleaned_data = cleaned_data.replace("Entering new CrewAgentExecutor chain", f":{self.colors[self.color_index]}[Entering new CrewAgentExecutor chain]")
+
+        if "Lead Data Specialistt" in cleaned_data:
+            # Apply different color 
+            cleaned_data = cleaned_data.replace("Lead Data Specialist", f":{self.colors[self.color_index]}[Lead Data Specialist]")
+            self.color_index = (self.color_index + 1) % len(self.colors)
+        if "Cultural Fit Analyst" in cleaned_data:
+            cleaned_data = cleaned_data.replace("Cultural Fit Analyst", f":{self.colors[self.color_index]}[Cultural Fit Analyst]")
+            self.color_index = (self.color_index + 1) % len(self.colors)
+        if "Lead Scorer and Validator" in cleaned_data:
+            cleaned_data = cleaned_data.replace("Lead Scorer and Validator", f":{self.colors[self.color_index]}[Lead Scorer and Validator]")
+            self.color_index = (self.color_index + 1) % len(self.colors)
+        if "Email Content Writer" in cleaned_data:
+            cleaned_data = cleaned_data.replace("Email Content Writer", f":{self.colors[self.color_index]}[Email Content Writer]")
+            self.color_index = (self.color_index + 1) % len(self.colors)
+        if "Engagement Optimization Specialist" in cleaned_data:
+            cleaned_data = cleaned_data.replace("Engagement Optimization Specialist", f":{self.colors[self.color_index]}[Engagement Optimization Specialist]")
+            self.color_index = (self.color_index + 1) % len(self.colors)
+        if "Finished chain." in cleaned_data:
+            cleaned_data = cleaned_data.replace("Finished chain.", f":{self.colors[self.color_index]}[Finished chain.]")
+            self.color_index = (self.color_index + 1) % len(self.colors)
+
+        self.buffer.append(cleaned_data)
+        if "\n" in data:
+            self.expander.markdown(''.join(self.buffer), unsafe_allow_html=True)
+            self.buffer = []
+
+# Creating Agents
+lead_data_agent = Agent(
+  config=agents_config['lead_data_agent'],
+  tools=[SerperDevTool(), ScrapeWebsiteTool()],
+  step_callback=StreamToExpander
+)
+
+cultural_fit_agent = Agent(
+  config=agents_config['cultural_fit_agent'],
+  tools=[SerperDevTool(), ScrapeWebsiteTool()],
+  step_callback=StreamToExpander
+)
+
+scoring_validation_agent = Agent(
+  config=agents_config['scoring_validation_agent'],
+  tools=[SerperDevTool(), ScrapeWebsiteTool()],
+  step_callback=StreamToExpander
+)
+
+# Creating Tasks
+lead_data_task = Task(
+  config=tasks_config['lead_data_collection'],
+  agent=lead_data_agent,
+)
+
+cultural_fit_task = Task(
+  config=tasks_config['cultural_fit_analysis'],
+  agent=cultural_fit_agent,
+)
+
+scoring_validation_task = Task(
+  config=tasks_config['lead_scoring_and_validation'],
+  agent=scoring_validation_agent,
+  context=[lead_data_task, cultural_fit_task],
+  output_pydantic=LeadScoringResult,
+)
+
+# Creating Crew
+lead_scoring_crew = Crew(
+  agents=[
+    lead_data_agent,
+    cultural_fit_agent,
+    scoring_validation_agent
+  ],
+  tasks=[
+    lead_data_task,
+    cultural_fit_task,
+    scoring_validation_task
+  ],
+  verbose=True
+)
+
+
+# Creating Agents
+email_content_specialist = Agent(
+  config=agents_config['email_content_specialist'],
+  step_callback=StreamToExpander
+)
+
+engagement_strategist = Agent(
+  config=agents_config['engagement_strategist'],
+  step_callback=StreamToExpander
+)
+
+# Creating Tasks
+email_drafting = Task(
+  config=tasks_config['email_drafting'],
+  agent=email_content_specialist,
+)
+
+engagement_optimization = Task(
+  config=tasks_config['engagement_optimization'],
+  agent=engagement_strategist,
+)
+
+# Creating Crew
+email_writing_crew = Crew(
+    agents=[
+    email_content_specialist,
+    engagement_strategist
+  ],
+  tasks=[
+    email_drafting,
+    engagement_optimization
+  ],
+  verbose=True
+)
+
+superbase_agent = Agent(
+  config=agents_config['superbase_agent'],
+  tools=[SerperDevTool(), ScrapeWebsiteTool()],
+  step_callback=StreamToExpander
+)
+
+# Creating Tasks
+lead_data_task = Task(
+  config=tasks_config['lead_data_collection'],
+  agent=lead_data_agent,
+)
+
+
+class SalesPipeline(Flow):
+    @start()
+    def fetch_leads(self):
+      # Specify the path to your Excel file
+      excel_file_path = "./sales_leads.csv"
+
+      # Read the Excel file
+      try:
+          leads_df = pd.read_csv(excel_file_path)
+      except FileNotFoundError:
+          raise FileNotFoundError(f"Excel file not found at {excel_file_path}. Please check the path.")
+      
+      # Convert the DataFrame to the required format
+      leads = []
+      for _, row in leads_df.iterrows():
+          lead = {
+              "lead_data": {
+                  "name": row["name"],
+                  "job_title": row["job_title"],
+                  "company": row["company"],
+                  "email": row["email"],
+                  "use_case": row["usecase"]
+              },
+          }
+          leads.append(lead)
+    
+      return leads
+    # def fetch_leads(self):
+        # Pull our leads from the database
+        # leads = [
+        #     {
+        #         "lead_data": {
+        #             "name": "Pavel Sher",
+        #             "job_title": "CEO of FuseBase",
+        #             "company": "FuseBase",
+        #             "email": "paul@fusebase.com",
+        #             "use_case": "Using AI Agent to do better data enrichment."
+        #         },
+        #     },
+        #                 {
+        #         "lead_data": {
+        #             "name": "Abdulaziz AlMulhem ",
+        #             "job_title": "Founder & CEO of packman.ai",
+        #             "company": "packman.ai",
+        #             "email": "Abdulaziz@packman.ai",
+        #             "use_case": "Using AI Agent for automation."
+        #         },
+        #     },
+        # ]
+        # return leads
+
+    @listen(fetch_leads)
+    def score_leads(self, leads):
+        scores = lead_scoring_crew.kickoff_for_each(leads)
+        self.state["score_crews_results"] = scores
+        return scores
+
+    @listen(score_leads)
+    def store_leads_score(self, scores):
+        # Here we would store the scores in the database
+        return scores
+
+    @listen(score_leads)
+    def filter_leads(self, scores):
+        return [score for score in scores if score['lead_score'].score >= 60]
+
+    @listen(filter_leads)
+    def write_email(self, leads):
+        scored_leads = [lead.to_dict() for lead in leads]
+        emails = email_writing_crew.kickoff_for_each(scored_leads)
+        #self.state.leads = leads
+        return emails
+
+    @listen(write_email)
+    def send_email(self, emails):
+      """
+      Sends an email with the given content to the specified recipient.
+
+      :param email_content: The content of the email (string).
+      :param recipient_email: The recipient's email address (string).
+      """
+      # for index, email in emails:
+      #   email_content = email[index]
+      #   lead = self.state.leads[index]["lead_data"].email
+      #   # Email configuration
+      #   smtp_server = 'smtp.gmail.com'
+      #   port = 587  # SMTP port for TLS
+      #   sender_email = 'adamelshimi@sensai-consulting.com'
+      #   sender_password = 'gldg ncwg kivt ofoq'
+
+      #   # Email subject
+      #   subject = f'Email - {lead}'
+
+      #   # Construct the email message
+      #   msg = EmailMessage()
+      #   msg['Subject'] = subject
+      #   msg['From'] = sender_email
+      #   msg['To'] = "adamelshimi@gmail.com"
+      #   msg.set_content(email_content)
+
+      #   # Send the email
+      #   try:
+      #       # Connect to the SMTP server
+      #       with smtplib.SMTP(smtp_server, port) as server:
+      #           server.starttls()  # Secure the connection
+      #           server.login(sender_email, sender_password)  # Login to your email account
+      #           server.send_message(msg)  # Send the email
+      #       print(f"Email sent successfully to {lead}!")
+      #   except Exception as e:
+      #       print(f"Failed to send email: {e}")
+
+      return emails
+# End of program
