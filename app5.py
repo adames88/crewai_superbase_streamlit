c78f6cfbd0ca5fee77f238b81353d1fa4df2a9a2
adames88 <adamelshimi1234@hotmail.co.uk>
2025-02-05 23:40:24 +0200

    adding stremlit secrets

--- app5.py
+++ app5.py
@@ -12,7 +12,14 @@ from IPython.display import HTML
 from flow_pipeline2 import StreamToExpander
 import sys
 import textwrap
+from openai import OpenAI
 
+# Set OpenAI API key from Streamlit secrets
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+# Set a default model
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-4o-mini"
 # Set OpenAI Model
 os.environ['OPENAI_MODEL_NAME'] = 'gpt-4o-mini'
 os.environ["SERPER_API_KEY"] = get_serper_api_key()
